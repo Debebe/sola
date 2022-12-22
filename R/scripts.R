c072935ed965d5690a87edc86fbce0a89582c7c9
Debebe <debebesh@gmail.com>
2022-12-22 12:58:34 +0000

    Add tmb_sampler

--- R/scripts.R
+++ R/scripts.R
@@ -204,3 +204,68 @@ clean_colnames   <- function(data){
 
 }
 
+#' Sample TMB fit
+#'
+#' @param fit The TMB fit
+#' @param nsample Number of samples
+#' @param rng_seed seed passed to set.seed.
+#' @param random_only Random only
+#' @param verbose If TRUE prints additional information.
+#'
+#' @return Sampled fit.
+#' @export
+sample_tmb <- function(fit, nsample = 100, rng_seed = NULL,
+                       random_only = TRUE, verbose = FALSE) {
+
+  set.seed(rng_seed)
+
+  stopifnot(methods::is(fit, "naomi_fit"))
+  stopifnot(nsample > 1)
+
+  to_tape <- TMB:::isNullPointer(fit$obj$env$ADFun$ptr)
+  if (to_tape)
+    fit$obj$retape(FALSE)
+
+  if(!random_only) {
+    if(verbose) print("Calculating joint precision")
+    hess <- sdreport_joint_precision(fit$obj, fit$par.fixed)
+
+    if(verbose) print("Inverting precision for joint covariance")
+    cov <- solve(hess)
+
+    if(verbose) print("Drawing sample")
+    ## TODO: write a version of rmvnorm that uses precision instead of covariance
+    smp <- mvtnorm::rmvnorm(nsample, fit$par.full, cov)
+
+  } else {
+    r <- fit$obj$env$random
+    par_f <- fit$par.full[-r]
+
+    par_r <- fit$par.full[r]
+    hess_r <- fit$obj$env$spHess(fit$par.full, random = TRUE)
+    smp_r <- rmvnorm_sparseprec(nsample, par_r, hess_r)
+
+    smp <- matrix(0, nsample, length(fit$par.full))
+    smp[ , r] <- smp_r
+    smp[ ,-r] <- matrix(par_f, nsample, length(par_f), byrow = TRUE)
+    colnames(smp)[r] <- colnames(smp_r)
+    colnames(smp)[-r] <- names(par_f)
+  }
+
+  if(verbose) print("Simulating outputs")
+  sim <- apply(smp, 1, fit$obj$report)
+
+  r <- fit$obj$report()
+
+  if(verbose) print("Returning sample")
+  fit$sample <- Map(vapply, list(sim), "[[", lapply(lengths(r), numeric), names(r))
+  is_vector <- vapply(fit$sample, inherits, logical(1), "numeric")
+
+  fit$sample[is_vector] <- lapply(fit$sample[is_vector], matrix, nrow = 1)
+  names(fit$sample) <- names(r)
+
+  fit
+}
+
+
+
